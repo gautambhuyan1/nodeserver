0911b1ed9bd33158d1c04986e268c12d8f76e919
gautam bhuyan <gautam@gautams-MacBook-Pro.local>
2016-09-18 22:40:58 -0400

    Updated server.js to add post handlers.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var app = require('express')();
 var lib = require('body-parser');
 
+app.use(lib.json());
+app.use(lib.urlencoded({extended:true}));
 
 app.get('/interests', function(req, res) {
     var interests = ["cricket", "soccer", "music", "photography"];
@@ -22,6 +24,22 @@ app.get('/messages/activity/:activity', function(req, res) {
     var messageList = {activity: activity}; 
     //console.log(messageList);
     res.json(messageList);
+});
+    
+app.post('/activity', function(req, res) {
+    var interest = req.body.interest;
+        lat = req.body.lat,
+        lng = req.body.lng,
+        activity = req.body.activity;
+    console.log({interest:interest, lat:lat, lng:lng});
+    res.json({done:true});
+});
+    
+app.post('/message', function(req, res) {
+    var activity = req.body.activity;
+        message = req.body.message;
+    console.log({activity:activity, message:message});
+    res.json({done:true});
 });
     
 app.use(function(req, res, next) {
