02d7c7f0ecc30b2a0e6176993eaf2b73b8ea30e9
gautam bhuyan <gautam@gautams-MacBook-Pro.local>
2016-09-18 22:19:26 -0400

    Added server.js and package.json

---
+++ server.js
@@ -0,0 +1,37 @@
+var app = require('express')();
+var lib = require('body-parser');
+
+
+app.get('/interests', function(req, res) {
+    var interests = ["cricket", "soccer", "music", "photography"];
+    res.json(interests);
+});
+    
+app.get('/activities/interest/:interest/lat/:lat/lng/:lng', function(req, res) {
+    var interest = req.params.interest;
+    var lat = req.params.lat;
+    var lng = req.params.lng;
+    var activityList = {interest : interest, lat: lat, lng: lng};
+    //console.log(activityList);
+    res.json(activityList);
+});
+    
+app.get('/messages/activity/:activity', function(req, res) {
+    var activity = req.params.activity;
+    //console.log(activity);
+    var messageList = {activity: activity}; 
+    //console.log(messageList);
+    res.json(messageList);
+});
+    
+app.use(function(req, res, next) {
+    res.writeHead(200, {'Content-type' : 'text/html'});
+    res.write("<h1> Hello world <\h1>");
+    next();
+});
+
+app.use(function(req, res) {
+    res.end("<h2> Bye bye world<\h2>");
+});
+
+app.listen(3030);
