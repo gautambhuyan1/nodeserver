0693a248c0c4874885c3b04179e91838bd789784
gautam bhuyan <gautam@gautams-MacBook-Pro.local>
2016-10-16 19:36:18 -0400

    New files

--- db.js
+++ db.js
@@ -1,17 +1,7 @@
 // real database and geospacial search
 
-var mongoClient = require('mongodb').MongoClient;
-var mongo = new mongoClient();
-
-var myDb = null;
-
-mongo.connect("mongodb://admin:secret@172.30.73.176:27017", function(err, db) {
-    myDb = db.db("sampledb");
-    console.log("db:connected");
-});
-
 // Get a list of valid interests
-exports.getInterests = function(callback) {
+exports.getInterests = function(myDb, callback) {
     console.log("@getInterests()");
     var interests = [];
     myDb.collection('interests', function(err, collection){
@@ -32,7 +22,7 @@ exports.getInterests = function(callback) {
 }
 
 // Add a new interest - Admin only
-exports.addInterest = function(myInterest) {
+exports.addInterest = function(myDb, myInterest) {
     console.log("@addInterests() "+ myInterest);
     var interests = [];
 
@@ -46,7 +36,7 @@ exports.addInterest = function(myInterest) {
 }
 
 // Get activities with certain interest and within 100 kms of location
-exports.getActivities = function(myInterest, myLat, myLng, callback) {
+exports.getActivities = function(myDb, myInterest, myLat, myLng, callback) {
     //console.log("@getActivities() "+ myInterest + " " + myPhone+ " " + myLat + " " + myLng);
     console.log("@getActivities()");
     //console.log(req);
@@ -79,7 +69,7 @@ exports.getActivities = function(myInterest, myLat, myLng, callback) {
 }
 
 // Create a new activity - store interest, location and activity
-exports.createActivity = function(myInterest, myActivity, myLat, myLng) {
+exports.createActivity = function(myDb, myInterest, myActivity, myLat, myLng) {
     console.log("@createActivities() "+myInterest+" "+myActivity+" "+myLat+" "+myLng);
     var activityToAdd = {interest:myInterest, activity: myActivity, location:[myLat, myLng]};
     var options = {w:1, wtimeout: 5000, journal:true, fsync:false};
@@ -91,15 +81,40 @@ exports.createActivity = function(myInterest, myActivity, myLat, myLng) {
 }
 
 // Get the messages associated with an activity
-exports.getMessages = function(activityId, phone, callback) {
-    console.log("@getMessages()"+activityId+" "+phone);
-    callback(0);
-
+exports.getMessages = function(myDb, myActivityId, callback) {
+    console.log("@getMessages()"+myActivityId);
+    var response;
+    var messages = [];
+    myDb.collection('messages', function(err, collection){
+        var query = {"activity": myActivityId};
+ 
+        //console.log(query);
+        var messageCursor = collection.find(query);
+        messageCursor.toArray(function(err, docArr){
+            for(doc in docArr) {
+                messages.push({"message":docArr[doc].message});
+                //console.log(messages);
+                //console.log({"message":docArr[doc].message});
+                //console.log("iteration ", doc);
+            }
+            //console.log(docArr);
+            //console.log(messages);
+            response = {activityid: myActivityId, messages: messages};
+            //console.log("Response:"+response);
+            callback(response);
+        });
+    });
 }
 
 // Store the messages associated with an activity
-exports.createMessage = function(activityId, msg, phone) {
-    console.log("@getMessages()"+activityId+" "+msg+" "+phone);
-
+exports.createMessage = function(myDb, activityId, msg) {
+    console.log("@createMessages()"+activityId+" "+msg);
+    var messageToAdd = {activity: activityId, message:msg};
+    var options = {w:1, wtimeout: 5000, journal:true, fsync:false};
+    myDb.collection('messages', function(err, collection) {
+        collection.insert(messageToAdd, options, function(err, results) {
+            console.log(results);
+        });
+    });
 }
 
